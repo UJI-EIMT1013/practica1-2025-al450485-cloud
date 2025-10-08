88df9b29fe56a49ceb6401bb73cd99607e873413
raulmanuelcireasa <al450485@uji.es>
2025-10-08 10:58:42 +0200

    V2: Haciendo el ejerciio2

--- Practica1.java
+++ Practica1.java
@@ -6,31 +6,61 @@ public class Practica1 {
 
     //EJERCICIO 1
     public static Set<Integer> multiplos (Iterator<Integer> it) {
-        //TODO
         Set<Integer> vistos = new HashSet<>();
         Set<Integer> resultado = new HashSet<>();
 
         while(it.hasNext()){
             Integer element = it.next();
-            vistos.add(element);
-            for(Integer iteraciones : vistos){
-                if(iteraciones % element == 0 || element % iteraciones == 0){
+            if(element != 0) {
+                if(vistos.contains(element)){
                     resultado.add(element);
+                }
+                else {
+                    vistos.add(element);
+                    for (int iteraciones : vistos) {
+                        if (iteraciones % element == 0 && iteraciones != element) {
+                            resultado.add(iteraciones);
+                        } else if (element % iteraciones == 0 && element != iteraciones){
+                            resultado.add(element);
+                        }
+                    }
                 }
             }
         }
-        return null;
+        return resultado;
     }
 
     //EJERCICIO2
     public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
         //TODO
+        Set<Integer> verd_cuadrados = new HashSet<>();
+        Set<Integer> verd_no_cuadrados = new HashSet<>();
+
+        for(int elemento : cuadrados){
+            for(int elemento2 : noCuadrados){
+                if (elemento*elemento == elemento2){
+                    verd_cuadrados.add(elemento);
+                }
+                else if(elemento2*elemento2 == elemento){
+                    verd_cuadrados.add(elemento2);
+                }
+            }
+            if(!verd_cuadrados.contains(elemento)){
+                verd_no_cuadrados.add(elemento);
+            }
+        }
+        cuadrados.clear();
+        cuadrados.addAll(verd_cuadrados);
+        noCuadrados.clear();
+        noCuadrados.addAll(verd_no_cuadrados);
 
     }
 
     //EJERCICIO 3
     public static<T> Collection<Set<T>> divideInSets (Iterator<T> it) {
         //TODO
+
+
         return null;
     }
 
