46c4ec86d893644f9d7ce6e2fd2cd60ab0b6b37b
raulmanuelcireasa <al450485@uji.es>
2025-10-08 09:52:27 +0200

    V1 Practica empezada

--- Practica1.java
+++ Practica1.java
@@ -7,6 +7,18 @@ public class Practica1 {
     //EJERCICIO 1
     public static Set<Integer> multiplos (Iterator<Integer> it) {
         //TODO
+        Set<Integer> vistos = new HashSet<>();
+        Set<Integer> resultado = new HashSet<>();
+
+        while(it.hasNext()){
+            Integer element = it.next();
+            vistos.add(element);
+            for(Integer iteraciones : vistos){
+                if(iteraciones % element == 0 || element % iteraciones == 0){
+                    resultado.add(element);
+                }
+            }
+        }
         return null;
     }
 
